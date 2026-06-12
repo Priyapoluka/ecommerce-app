3ca5f568404a3b15c5d9ca532ed9214333577cf8
Priya Poluka <yagnapriya2409@gmail.com>
2026-06-12 09:20:39 +0530

    Fix orders page

---
+++ app/orders/page.tsx
@@ -0,0 +1,9 @@
+export default function OrdersPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Orders Page
+      </h1>
+    </div>
+  );
+}
